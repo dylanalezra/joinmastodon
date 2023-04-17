4dfd2c56af81d19144a7b36460580138582b3fc6
Dylan Alezra <dylan.alezra@gmail.com>
2023-04-17 15:35:42 +0200

    Update sponsors.ts

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -4,10 +4,177 @@ const sponsors = {
   additionalFunding: [],
   platinum: [
     {
+<<<<<<< HEAD
       url: "https://www.weid.education/",
       logo: require("../public/sponsors/weid.png"),
       name: "Weid",
     }, 
+=======
+      url: "https://www.fastly.com/",
+      logo: require("../public/sponsors/fastly.svg"),
+      name: "Fastly",
+    }, // Custom/other
+    {
+      url: "https://www.datadoghq.com",
+      logo: require("../public/sponsors/datadog.png"),
+      name: "Datadog",
+    }, // Custom/other
+    {
+      url: "https://mobi.com/",
+      logo: require("../public/sponsors/mobi-across-big.png"),
+      name: "Mobi",
+    }, // Custom
+    {
+      url: "https://medium.com/",
+      logo: require("../public/sponsors/medium.png"),
+      name: "Medium",
+    }, // Custom
+    {
+      url: "https://oak.is/",
+      logo: require("../public/sponsors/oak.svg"),
+      name: "Oak Studios",
+    }, // Custom/other
+    {
+      url: "https://www.ramotion.com/agency/ui-ux-design",
+      logo: require("../public/sponsors/truevendor.png"),
+      name: "Ramotion",
+    },
+    {
+      url: "https://airvpn.org/landing/mastodon",
+      logo: require("../public/sponsors/airvpn.webp"),
+      name: "AirVPN",
+    },
+    {
+      url: "https://www.firesticktricks.com/",
+      logo: require("../public/sponsors/firesticktricks.webp"),
+      name: "Fire Stick Tricks",
+    },
+    {
+      url: "https://papersowl.com/write-my-essay-please",
+      logo: require("../public/sponsors/papersowl.png"),
+      name: "Papers Owl",
+    },
+    {
+      url: "https://sextoycollective.com",
+      logo: require("../public/sponsors/sextoycollective.webp"),
+      name: "Sex Toy Collective",
+    },
+    {
+      url: "https://www.sister-sites.co.uk/",
+      logo: require("../public/sponsors/sistersites-logo.png"),
+      name: "sister-sites.co.uk",
+    }, // Custom
+    {
+      url: "https://masto.host/",
+      logo: require("../public/sponsors/MH-horizontal-white.png"),
+      name: "Masto.host - Fully managed Mastodon hosting",
+    }, // Custom
+    {
+      url: "https://assemblyfour.com/",
+      logo: require("../public/sponsors/a4.webp"),
+      name: "Assembly Four",
+    }, // Custom
+    {
+      url: "https://www.auscasinos.net/",
+      logo: require("../public/sponsors/auscasinos.png"),
+      name: "Auscasinos - Online casinos in Australia",
+    }, // Custom
+    {
+      url: "https://www.globenewswire.com/news-release/2021/04/07/2206309/0/en/10-Best-Online-Casinos-in-Australia-List-of-Real-Money-Pokies-Sites-Rated-By-Users-and-Popularity.html",
+      logo: require("../public/sponsors/alt-casino-1.png"),
+      name: "Online Casinos Australia",
+    }, // Custom
+    {
+      url: "https://www.nettikasinot.org/",
+      logo: require("../public/sponsors/nettikasinot.svg"),
+      name: "Nettikasinot",
+    },
+    {
+      url: "https://wideangle.co",
+      logo: require("../public/sponsors/wide-angle-analytics.webp"),
+      name: "Wide Angle Analytics",
+    }, // Custom
+    {
+      url: "https://www.noneedtostudy.com/take-my-online-class/",
+      logo: require("../public/sponsors/noneedtostudy.com-logo-wide.png"),
+      name: "Experts Help Take My Online Classes - NoNeedToStudy.com",
+    }, // Custom
+    {
+      url: "https://www.movavi.com/imovie-for-windows.html",
+      logo: require("../public/sponsors/movavi.svg"),
+      name: "Movavi",
+    }, // Custom/other
+    {
+      url: "https://krystal.uk/",
+      logo: require("../public/sponsors/Krystal_Logo.svg"),
+      name: "Krystal Hosting",
+    }, // Custom
+    {
+      url: "https://www.raspberrypi.com/",
+      logo: require("../public/sponsors/Raspberry-Pi-Logo.png"),
+      name: "Raspberry Pi",
+    }, // Custom
+    {
+      url: "https://bibly.com/",
+      logo: require("../public/sponsors/bibly-logo.png"),
+      name: "Bibly",
+    }, // Custom
+    {
+      url: "https://www.zerotier.com/",
+      logo: require("../public/sponsors/zerotier_logo_black.png"),
+      name: "ZeroTier, Inc.",
+    }, // Custom
+    {
+      url: "https://www.goldeneaglecoin.com/",
+      logo: require("../public/sponsors/ge-logo-coin-card2.png"),
+      name: "Golden Eagle Coins",
+    }, // Custom
+    {
+      url: "https://apernet.io/",
+      logo: require("../public/sponsors/AperNetLogo.svg"),
+      name: "Aperture Internet Laboratory",
+    }, // Custom
+    {
+      url: "https://rubycentral.org/",
+      logo: require("../public/sponsors/ruby-central.jpeg"),
+      name: "Ruby Central",
+    }, // Custom
+    {
+      url: "https://opalstack.com/?utm_campaign=mastodon2022&utm_source=joinmastodon&utm_term=mastodon%2Bhosting",
+      logo: require("../public/sponsors/opal-stack.svg"),
+      name: "Managed Hosting for developers, entrepreneurs, and businesses like yours.",
+    }, // Custom
+    {
+      url: "https://allbursaries.co.za/",
+      logo: require("../public/sponsors/AMP All Bursaries South Africa.png"),
+      name: "AllBursaries",
+    }, // Custom
+    {
+      url: "https://theoatmeal.com/",
+      logo: require("../public/sponsors/oatmeal_logo_2020_square@5x.png"),
+      name: "The Oatmeal",
+    }, // Custom
+    {
+      url: "https://www.completewebresources.com/",
+      logo: require("../public/sponsors/complete-seo-logo-padding.png"),
+      name: "Complete SEO",
+    }, // Custom
+    {
+      url: "https://intellifluence.com/",
+      logo: require("../public/sponsors/intellifluence logos.png"),
+      name: "Intellifluence supports all influencers",
+    }, // Custom
+    {
+      url: "https://toot.io/mastodon_hosting.html",
+      logo: require("../public/sponsors/toot_io_logo.png"),
+      name: "toot.io",
+    }, // Custom
+    {
+      url: "https://www.daniweb.com/",
+      logo: require("../public/sponsors/daniweb.png"),
+      name: "DaniWeb",
+    }, // Custom
+>>>>>>> dc1bbe853eb1d7e6d6bebdc294cc1fd0169acfe7
   ],
   gold: [
   ],
